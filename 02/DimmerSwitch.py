65316b8ec64a9b67bd8dc368ce5022d2c77cd9e8
DevKantoran <devkantoran@outlook.com>
2023-12-28 19:51:44 +0700

    Chapter 02 finished

---
+++ 02/DimmerSwitch.py
@@ -0,0 +1,26 @@
+# DimmerSwitch class
+
+
+class DimmerSwitch:
+    def __init__(self):
+        self.switchIsOn = False
+        self.brightness = 0
+
+    def turnOn(self):
+        self.switchIsOn = True
+
+    def turnOff(self):
+        self.switchIsOn = False
+
+    def raiseLevel(self):
+        if self.brightness < 10:
+            self.brightness = self.brightness + 1
+
+    def lowerLevel(self):
+        if self.brightness > 0:
+            self.brightness = self.brightness - 1
+
+    # Extra method for debugging
+    def show(self):
+        print("Switch is on?", self.switchIsOn)
+        print("Brightness is:", self.brightness)
---
+++ 02/OO_DimmerSwitch_with_Test_Code.py
@@ -0,0 +1,52 @@
+# DimmerSwitch class with test code
+
+
+class DimmerSwitch:
+    def __init__(self):
+        self.switchIsOn = False
+        self.brightness = 0
+
+    def turnOn(self):
+        self.switchIsOn = True
+
+    def turnOff(self):
+        self.switchIsOn = False
+
+    def raiseLevel(self):
+        if self.brightness < 10:
+            self.brightness = self.brightness + 1
+
+    def lowerLevel(self):
+        if self.brightness > 0:
+            self.brightness = self.brightness - 1
+
+    # Extra method for debugging
+    def show(self):
+        print("Switch is on?", self.switchIsOn)
+        print("Brightness is:", self.brightness)
+
+
+# Main code
+oDimmer = DimmerSwitch()
+
+# Turn switch on, and raise the level 5 times
+oDimmer.turnOn()
+oDimmer.raiseLevel()
+oDimmer.raiseLevel()
+oDimmer.raiseLevel()
+oDimmer.raiseLevel()
+oDimmer.raiseLevel()
+oDimmer.show()
+
+# Lower the level 2 times, and turn off
+oDimmer.lowerLevel()
+oDimmer.lowerLevel()
+oDimmer.turnOff()
+oDimmer.show()
+
+# Turn switch on, and raise the level 3 times
+oDimmer.turnOn()
+oDimmer.raiseLevel()
+oDimmer.raiseLevel()
+oDimmer.raiseLevel()
+oDimmer.show()
