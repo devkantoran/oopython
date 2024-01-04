a43039cf1a6d39dc36f83d4881e690475e9db08c
DevKantoran <devkantoran@outlook.com>
2024-01-04 23:24:08 +0700

    Chapter 7 done

---
+++ 07/OrderPizzaWithNone.py
@@ -0,0 +1,34 @@
+def orderPizza(size, style="regular", topping=None):
+    # Do some calculations based on the size and style
+    # Check if a topping was specified
+    PRICE_OF_TOPPING = 1.50  # price for any topping
+
+    if size == "small":
+        price = 10.00
+    elif size == "medium":
+        price = 14.00
+    else:  # large
+        price = 18.00
+
+    if style == "deepdish":
+        price = price + 2.00  # charge extra for deepdish
+
+    line = "You have ordered a " + size + " " + style + " pizza with "
+    if topping is None:  # check if no topping was passed in
+        print(line + "no topping")
+    else:
+        print(line + topping)
+        price = price + PRICE_OF_TOPPING
+
+    print("The price is $", price)
+    print()
+
+
+# You could order a pizza in the following ways:
+orderPizza("large")  # large, defaults to regular, no topping
+
+orderPizza("large", style="regular")  # same as above
+
+orderPizza("medium", style="deepdish", topping="mushrooms")
+
+orderPizza("small", topping="mushrooms")  # style defaults to regular
---
+++ 07/PygameDemo10_ImageTextButtonBall/PygameTextButtonBall.py
@@ -0,0 +1,150 @@
+# Pygame demo text and buttons
+
+# 1 - Import packages
+import pygame
+from pygame.locals import *
+import sys
+import random
+import pygwidgets
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+print(BASE_PATH / "images/ball.png")
+
+
+# 2 - Define constants
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
+WHITE = (255, 255, 255)
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+FRAMES_PER_SECOND = 30
+N_PIXELS_PER_FRAME = 3
+BALL_WIDTH_HEIGHT = 100
+
+# 3 - Initialize the world
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+# 4 - Load assets: image(s), sounds,  etc.
+
+# 5 - Initialize variables
+oBall = pygwidgets.Image(
+    window,
+    (0, 0),
+    "07/PygameDemo10_ImageTextButtonBall/images/ball.png",
+)
+ballLeft = 0
+ballTop = 0
+
+MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
+MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
+ballX = random.randrange(MAX_WIDTH)
+ballY = random.randrange(MAX_HEIGHT)
+xSpeed = N_PIXELS_PER_FRAME
+ySpeed = N_PIXELS_PER_FRAME
+
+oBackground = pygwidgets.Image(
+    window, (0, 0), "07/PygameDemo10_ImageTextButtonBall/images/background.jpg"
+)
+
+oRestartButton = pygwidgets.CustomButton(
+    window,
+    (500, 430),
+    up=BASE_PATH / "images/restartUp.png",
+    down=BASE_PATH / "images/restartDown.png",
+    over=BASE_PATH / "images/restartOver.png",
+    disabled=BASE_PATH / "images/restartDisabled.png",
+)
+
+oHitMeButton = pygwidgets.TextButton(window, (500, 370), "Hit Me")
+
+oMessageTextA = pygwidgets.DisplayText(
+    window, (20, 50), "Here is some text", fontSize=36, textColor=WHITE
+)
+
+oMessageTextB = pygwidgets.DisplayText(
+    window,
+    (20, 150),
+    "Here is more text\nAnd more text\nAnd even more text",
+    fontSize=36,
+    textColor=WHITE,
+    justified="center",
+)
+
+oUserInputA = pygwidgets.InputText(
+    window, (20, 350), "", fontSize=24, textColor=BLACK, backgroundColor=WHITE
+)
+
+oUserInputB = pygwidgets.InputText(
+    window,
+    (20, 430),
+    "",
+    width=400,
+    fontSize=24,
+    textColor=WHITE,
+    backgroundColor=BLACK,
+)
+
+counter = 0
+
+# 6 - Loop forever
+while True:
+    # 7 - Check for and handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if oRestartButton.handleEvent(event):
+            counter = 0
+
+        if oHitMeButton.handleEvent(event):
+            print("Do not hit me")
+
+        if oUserInputA.handleEvent(event):
+            userText = oUserInputA.getText()
+            print("In the first field, the user entered:", userText)
+
+        if oUserInputB.handleEvent(event):
+            userText = oUserInputB.getText()
+            print("The the second field, the user entered:", userText)
+
+    # 8 - Do any "per frame" actions
+    counter = counter + 1
+    oMessageTextA.setValue("Here is some text.  Loop counter:" + str(counter))
+
+    ballLeft, ballRight = oBall.getLoc()
+
+    if (ballLeft < 0) or (ballLeft + BALL_WIDTH_HEIGHT >= WINDOW_WIDTH):
+        xSpeed = -xSpeed  # reverse X direction
+
+    if (ballTop < 0) or (ballTop + BALL_WIDTH_HEIGHT >= WINDOW_HEIGHT):
+        ySpeed = -ySpeed  # reverse Y direction
+
+    # Update the rectangle of the ball, based on the speed in two directions
+    ballLeft = ballLeft + xSpeed
+    ballTop = ballTop + ySpeed
+    oBall.setLoc((ballLeft, ballTop))
+
+    # 9 - Clear the window before drawing it again
+    oBackground.draw()  # draw a background image
+
+    # 10 - Draw the window elements
+    oBall.draw()
+
+    oRestartButton.draw()
+    oHitMeButton.draw()
+
+    oMessageTextA.draw()
+    oMessageTextB.draw()
+
+    oUserInputA.draw()
+    oUserInputB.draw()
+
+    # 11 - Update the window
+    pygame.display.update()
+
+    # 12 - Slow things down
+    clock.tick(FRAMES_PER_SECOND)  # make pygame wait
